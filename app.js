6fd111cdd657b91dc8f0344e172f75a2d6e7bf6e
LocksiDesmond <Des.olanrewaju@gmail.com>
2020-07-16 22:19:02 +0100

    removed cores

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT;
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send(
