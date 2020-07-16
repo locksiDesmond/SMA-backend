633e7e1089b33b26e6fd8b97fc64fd379ac89368
LocksiDesmond <Des.olanrewaju@gmail.com>
2020-07-16 22:15:07 +0100

    bg- fix issues (cors not installed ) ft- added profile

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const dotenv = require("dotenv").config();
 const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send(
+    "Hello world, this is the home page of SMA. you should send a post request to '/' with your data "
+  );
+});
 app.post("/", (req, res) => {
   const body = req.body;
   res.send({ body });
 });
-app.listen(PORT);
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
